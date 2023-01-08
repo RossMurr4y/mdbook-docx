6ee2b6f452cbdaf83c42a88f68b0630c4e0cd358
Ross Murray <rossmurray@protonmail.com>
2023-01-08 19:01:07 +1100

    refactor: clean slate to rewrite w/o pandoc

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,4 @@
-use anyhow::Result;
-use log::error;
-use mdbook::renderer::RenderContext;
-use std::io::{self};
-
-mod logs;
-mod document;
-mod pandoc;
-
-fn run() -> Result<()> {
-    let mut stdin = io::stdin();
-    let ctx = RenderContext::from_json(&mut stdin)
-        .expect("Error with the input data. Is everything formatted correctly?");
-
-    let list: document::DocumentList = ctx
-        .config
-        .get_deserialized_opt("output.docx")
-        .expect("Error reading \"output.docx\" configuration in book.toml. Check that all values are of the correct data type.")
-        .unwrap_or_default();
-
-    list.process(ctx.clone())
-}
 
 fn main() {
-    logs::init_logger();
-
-    let r = run();
-    if let Err(e) = r {
-        error!("An error has occurred while creating the document.\n{}", e);
-    }
+    println!("Hello, world!");
 }
