7ea1f2c7bf79174ff1117419e171a66538643108
Ross Murray <rossmurray@protonmail.com>
2023-01-08 19:10:12 +1100

    feat: init Style struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// A text formatting style definition.
+struct Style {
+    // the name that the style is refrenced by in a section configuration
+    alias: String,
+    // the font family for the style
+    font: String,
+    // the font size for the style
+    size: u32,
+    // the font color for the style
+    color: String,
+}
+
 
 fn main() {
     println!("Hello, world!");
