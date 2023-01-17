7ca10851ecc09eb4e25d0acc75d7ab222b83fa50
Ross Murray <rossmurray@protonmail.com>
2023-01-17 17:10:31 +1100

    feat: define stylesdefaults and additional style sets in Styles.toml, with module-provided defaults for any missing

--- src/main.rs
+++ src/main.rs
@@ -54,35 +54,72 @@ impl TryFrom<markdown::Span> for Span {
 }
 
 // A text formatting style definition.
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Default, Deserialize, Debug)]
 struct Style {
     // the name that the style is refrenced by in a section configuration
+    #[serde(default = "Style::default_alias")]
     alias: String,
     // the font family for the style
+    #[serde(default = "Style::default_font")]
     font: String,
     // the font size for the style
+    #[serde(default = "Style::default_size")]
     size: u32,
     // the font color for the style
+    #[serde(default = "Style::default_color")]
     color: String,
 }
 
 // implement default values for the Style struct
-impl Default for Style {
-    fn default() -> Self {
+impl Style {
+
+    // default function used by serde to populate the default value
+    // for the default field when not defined in the Styles.toml file
+    fn default_new() -> Style {
         Style {
-            alias: String::from("default"),
-            font: String::from("sans-serif"),
-            size: 12,
-            color: String::from("#000000"),
+            alias: Style::default_alias(),
+            font: Style::default_font(),
+            size: Style::default_size(),
+            color: Style::default_color(),
         }
     }
+    // default function used by serde to populate the default value
+    // for the alias field when not defined in the Styles.toml file
+    fn default_alias() -> String { "default".to_string() }
+    // default function used by serde to populate the default value
+    // for the font field when not defined in the Styles.toml file
+    fn default_font() -> String { "sans-serif".to_string() }
+    // default function used by serde to populate the default value
+    // for the size field when not defined in the Styles.toml file
+    fn default_size() -> u32 { 12 }
+    // default function used by serde to populate the default value
+    // for the color field when not defined in the Styles.toml file
+    fn default_color() -> String { "#000000".to_string() }
 }
 
-impl Style {
+// A Styles configuration including a default with optional additional
+// style definitions. Values that are not defined for a specific style 
+// are populated from the default style. If the default style has 
+// missing values then the default values defined on the Style struct
+// are used.
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Styles {
+    // an individual default style that is used to populate missing values in all other styles
+    #[serde(default = "Style::default_new")]
+    default: Style,
+    // a vector of styles that can be referenced by alias in a section configuration
+
+    #[serde(default = "Styles::empty_styles")]
+    style: Vec<Style>,
+}
+impl Styles {
+
+    // default function used by serde to populate the default value
+    fn empty_styles() -> Vec<Style> { Vec::new() }
 
     // looks for a Styles.toml file in the current directory and 
     // deserialises the file into a vector of Style structs
-    fn get_styles() -> Vec<Style> {
+    fn get_styles() -> Styles {
 
         // current directory
         let current_dir = std::env::current_dir()
@@ -93,20 +130,18 @@ impl Style {
             .to_str()
             .expect("Failed to parse the parent directory to a string.");
 
-        let styles = format!("{}/Styles.toml", src_dir);
-        // debug print the styls_file path
-        println!("Styles file: {}", styles);
+        let path = format!("{}/Styles.toml", src_dir);
+        // debug print the path
+        println!("Styles file: {}", path);
 
         // read an existing Styles.toml or create it and populate it with the default style
-        match std::fs::read_to_string(styles) {
+        match std::fs::read_to_string(path) {
             Ok(s) => {
                 return toml::from_str(&s)
                     .expect("Failed to parse Styles.toml - check the file is valid TOML.");
             },
             Err(_) => {
-                let mut styles : Vec<Style> = Vec::new();
-                styles.push(Style::default());
-                return styles;
+                return Styles::default();
             }
         };
     }
@@ -128,7 +163,7 @@ struct Section {
 use std::io;
 use mdbook::renderer::RenderContext;
 fn main() {
-    let styles = Style::get_styles();
+    let styles: Styles = Styles::get_styles();
     println!("{:#?}", styles);
 
     let mut stdin = io::stdin();
@@ -136,5 +171,5 @@ fn main() {
     let ctx = RenderContext::from_json(&mut stdin).unwrap();
 
 
-    println!("{:?}", ctx);
+    println!("{:#?}", ctx);
 }
